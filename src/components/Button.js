4dbe8c13a50ad784dabd386722839d36883c4a6f
renitagid <renita.y.ip@gmail.com>
2022-11-14 11:56:18 -0800

    rest button user story is functional

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,13 @@
 import React from 'react'
 
-const Button = () => {
+const Button = (props) => {
 
+    const handleClick = () => {
+        props.reset()
+    }
   return (
     <>
-    <button>Button</button>
+    <button className="button" onClick={handleClick}>Reset Game</button>
     </>
   )
 }
