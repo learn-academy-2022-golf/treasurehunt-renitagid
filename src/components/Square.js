591a05c0e4b532cb7191bfec9811e0caa49b40e0
renitagid <renita.y.ip@gmail.com>
2022-11-14 10:38:02 -0800

    alert index user story completed

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,10 +1,18 @@
-import React from "react"
+import React from "react";
+import { findAllInRenderedTree } from "react-dom/test-utils";
 
 const Square = (props) => {
+  const handleClick = () => {
+    props.handleGamePlay(props.index);
+  };
+
   return (
     <>
-      <div className="square">{props.value}</div>
+      <div className="square" onClick={handleClick}>
+        {" "}
+        {props.value}
+      </div>
     </>
-  )
-}
-export default Square
+  );
+};
+export default Square;
